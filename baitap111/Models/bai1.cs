8efaf1962e3b06f52f2ef37de2e1fdea59e0d46c
TranThiHue999 <tranthihue20899@gmail.com>
2021-12-21 12:14:46 +0700

    1

--- baitap111/Models/bai1.cs
+++ baitap111/Models/bai1.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace baitap111.Models
 {
+    [Table ("bài1")]
     public class bai1
     {
+        [Key]
+        public int mã { get; set; }
+        public string tên { get; set; }
         //bài 1
     }
 
